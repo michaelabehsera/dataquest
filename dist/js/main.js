b4dfab4391d15de980cb8265d755088c8c1dbdcf
gatilin222 <tetris2282>
2015-08-18 11:17:20 +0300

    init commit

---
+++ dist/js/main.js
@@ -0,0 +1,137 @@
+var $body,
+	window_height,
+	window_width,
+	header,
+	header_menu,
+	header_menu_height,
+	learn_section,
+	learn_offset,
+	$user_menu_butt,
+
+	current_slide,
+	slides,
+	prev_slide,
+	next_slide,
+	next_2_slide,
+
+	media_point_1 = 1024,
+	media_point_2 = 768,
+	media_point_3 = 480,
+	media_point_4 = 320;
+
+
+$(document).ready(function ($) {
+	$body = $('body');
+	header = $('#header');
+	header_menu = $('#head_menu_block');
+	$user_menu_butt = $('#user_menu_butt');
+	learn_section = $('#learn_section_w');
+
+
+
+	$('.scrollTo').on('click', function () {
+		header_menu_height = header_menu.height();
+		var firedEl = $(this);
+
+		docScrollTo($(firedEl.attr("href")).offset().top - header_menu_height, 1000);
+
+		return false;
+	});
+
+	$user_menu_butt.on('click', function () {
+		if ($body.hasClass('menu_open')) {
+			$body.removeClass('menu_open');
+			$(this).removeClass('active_mod');
+		} else {
+			$body.addClass('menu_open');
+			$(this).addClass('active_mod');
+		}
+	});
+
+	scroll_function();
+});
+
+$(window).on('load', function () {
+	window_height = $(window).height();
+	window_width = $(window).width();
+	header_menu_height = header_menu.height();
+	learn_offset = learn_section.offset().top;
+
+	fadeSliderInit($('#head_slider'));
+
+	vertSliderInit($('#reply_slider'));
+
+	move_to_block('#learn_section_w', '#learn_scroll_link');
+});
+
+$(window).on('scroll', function () {
+	scroll_function();
+});
+
+
+function scroll_function() {
+	window_height = $(window).height();
+	window_width = $(window).width();
+
+	if ($(window).scrollTop() > (learn_offset - header_menu_height)) {
+		header.addClass('fixed_mod');
+	} else {
+		header.removeClass('fixed_mod');
+	}
+}
+
+function fadeSliderInit(target) {
+	target.bxSlider({
+		mode:'fade',
+		controls:false,
+		//auto:true,
+		speed:0,
+		onSlideBefore: function ($slideElement, oldIndex, newIndex) {
+			current_slide = $slideElement;
+			slides = target.find('li');
+			prev_slide = current_slide.prev();
+			next_slide = current_slide.next();
+			next_2_slide = next_slide.next();
+
+			slides.removeClass('active_slide next_slide next_sec_slide prev_slide');
+			prev_slide.addClass('prev_slide');
+			current_slide.addClass('active_slide');
+			next_slide.addClass('next_slide');
+			next_2_slide.addClass('next_sec_slide');
+		}
+	});
+}
+
+function vertSliderInit(target) {
+	target.bxSlider({
+		mode:'vertical',
+		controls:false,
+		pager:false,
+		auto:true,
+		onSlideNext: function ($slideElement, oldIndex, newIndex) {
+			$slideElement.addClass('active_slide').siblings().removeClass('active_slide');
+		}
+	});
+}
+
+function move_to_block(block, control) {
+	var $block = $(block),
+		$control = $(control);
+	$control.on('click', function () {
+		var $block_pos = $block.offset();
+		$('body,html').animate({
+			scrollTop: $block_pos.top - header_menu_height
+		}, 500);
+		return false;
+	})
+}
+
+function docScrollTo(pos, speed, callback) {
+
+	$('html,body').animate({'scrollTop': pos}, speed);
+
+	if (typeof(callback) == 'function') {
+		callback();
+	}
+
+}
---
+++ gulpfile.js
@@ -0,0 +1,154 @@
+var gulp = require('gulp'),
+	clean = require('gulp-clean'),
+	jade = require('gulp-jade'),
+	sass = require('gulp-sass'),
+	inlineimage = require('gulp-inline-image'),
+	prefix = require('gulp-autoprefixer'),
+	plumber = require('gulp-plumber'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	uncss = require('gulp-uncss'),
+	csso = require('gulp-csso'),
+	dirSync = require( 'gulp-directory-sync'),
+	browserSync = require('browser-sync').create(),
+	purify = require('gulp-purifycss');
+
+var assetsDir = 'assets/';
+var outputDir = 'dist/';
+var buildDir = 'build/';
+
+//----------------------------------------------------Compiling
+gulp.task('jade', function() {
+	gulp.src([assetsDir+'jade/*.jade', '!'+assetsDir+'jade/_*.jade'])
+		.pipe(plumber())
+		.pipe(jade({pretty:true}))
+		.pipe(gulp.dest(outputDir))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+	gulp.src([assetsDir+'sass/**/*.scss', '!'+assetsDir+'sass/**/_*.scss'])
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(inlineimage())
+		.pipe(prefix('last 3 versions'))
+		.pipe(gulp.dest(outputDir + 'styles/'))
+		.pipe(browserSync.stream());
+});
+//----------------------------------------------------Compiling###
+
+//-------------------------------------------------Synchronization
+gulp.task('imageSync', function () {
+	return gulp.src( '' )
+		.pipe(plumber())
+		.pipe(dirSync(assetsDir+'i/', outputDir+'i/', { printSummary: true } ))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('fontsSync', function () {
+	return gulp.src('')
+		.pipe(plumber())
+		.pipe(dirSync(assetsDir+'fonts/', outputDir+'fonts/', { printSummary: true } ))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('jsSync', function () {
+	return gulp.src('')
+		.pipe(plumber())
+		.pipe(dirSync(assetsDir+'js/', outputDir+'js/', { printSummary: true } ))
+		.pipe(browserSync.stream());
+});
+//-------------------------------------------------Synchronization###
+
+
+//watching files and run tasks
+gulp.task('watch', function () {
+	gulp.watch(assetsDir+'jade/**/*.jade', ['jade']);
+	gulp.watch(assetsDir+'sass/**/*.scss', ['sass']);
+	gulp.watch(assetsDir+'js/*.js', ['jsSync']);
+	gulp.watch(assetsDir+'i/**/*',['imageSync']);
+	gulp.watch(assetsDir+'fonts/**/*',['fontsSync']);
+});
+
+//livereload and open html in browser
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		port:1337,
+		server: {
+			baseDir: outputDir
+		}
+	});
+});
+
+
+//---------------------------------building final project folder
+//clean build folder
+gulp.task('cleanBuildDir',function() {
+	return gulp.src(buildDir)
+		.pipe(clean());
+});
+
+//minify images
+gulp.task('imgBuild',function() {
+	return gulp.src(outputDir+'i/**/*')
+	.pipe(imagemin({
+		progressive: true,
+		svgoPlugins: [{removeViewBox: false}],
+		use: [pngquant()]
+	}))
+	.pipe(gulp.dest(buildDir + 'i/'))
+});
+
+//copy fonts
+gulp.task('fontsBuild',function() {
+	return gulp.src(outputDir+'fonts/**/*')
+		.pipe(gulp.dest(buildDir + 'fonts/'))
+});
+
+//copy html
+gulp.task('htmlBuild',function() {
+	return gulp.src(outputDir+'**/*.html')
+		.pipe(gulp.dest(buildDir))
+});
+
+//copy js
+gulp.task('jsBuild', function() {
+	return gulp.src(outputDir+'js/**/*')
+		.pipe(gulp.dest(buildDir + 'js/'))
+});
+
+//copy and minify css
+gulp.task('cssBuild', function() {
+	return gulp.src(outputDir+'styles/**/*')
+		.pipe(purify([outputDir+'js/**/*', outputDir+'**/*.html']))
+		// .pipe(csso())
+		.pipe(gulp.dest(buildDir + 'styles/'))
+});
+
+
+//--------------------------------------------If you need iconfont
+var iconfont = require('gulp-iconfont'),
+	iconfontCss = require('gulp-iconfont-css'),
+	fontName = 'rtr_iconfont';
+gulp.task('iconfont', function(){
+	gulp.src(['assets/i/icons/*.svg'])
+		.pipe(iconfontCss({
+			path: 'assets/sass/_icons_template.scss',
+			fontName: fontName,
+			targetPath: '../../sass/_icons.scss',
+			fontPath: '../fonts/icons/',
+			svg:true
+		}))
+		.pipe(iconfont({
+			fontName: fontName,
+			svg:true,
+			formats: ['ttf', 'eot', 'woff','svg']
+		}))
+		.pipe(gulp.dest('assets/fonts/icons'));
+});
+
+gulp.task('default',['jade','sass','imageSync','fontsSync','jsSync','watch','browser-sync']);
+
+gulp.task('build',['cleanBuildDir'],function() {
+	gulp.start('imgBuild','fontsBuild','htmlBuild','jsBuild','cssBuild');
+});
